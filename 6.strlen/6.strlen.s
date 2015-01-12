fead8c3150c607806bc44f2e5216b466ba4a20b2
Simon Whitehead <chemnova@gmail.com>
2015-01-12 23:02:03 +1100

    Added a scasb version of strlen to #6

--- 6.strlen/6.strlen.s
+++ 6.strlen/6.strlen.s
@@ -4,13 +4,15 @@
 ; 6. strlen
 ;
 ;    This program prints "Hello world!" but it figures out the length
-;    of the string all by itself. This can be done wish scasb too (which I
-;    may also implement as an example), but it is actually quicker to
-;    handle roll this than to use the scasX functions.
+;    of the string all by itself.
+;
+;    The strlen function is a hand rolled function to find the null
+;    terminator. The strlen_scasb function uses the scasb instruction
+;    to search for the null terminator.
 
 section .data
 
-    msg db "Hello world!", 10, 0
+    msg db "Hello world!", 10, 0	; The input string - including a newline character
 
     ; Syscall information
     sys_write equ 4
@@ -34,10 +36,24 @@ _start:
 
     int 0x80
 
+    push msg
+    call strlen_scasb
+
+    mov rdx,rax
+    mov rax,sys_write
+    mov rbx,stdout
+    mov rcx,msg
+
+    int 0x80
+
     mov rax,sys_exit
     mov rbx,0
 
     int 0x80
+
+
+; This version of strlen manually loops over 
+; the input string to find the null terminator
 
 strlen:
 
@@ -59,6 +75,28 @@ strlen:
 .endloop:
 
     mov rax,[rbp-4]	; Store the value of our local counter in rax
+
+    leave
+    ret
+
+; This version of strlen uses the scasb instruction
+; to scan the input string for the null terminator.
+; It also utilises the stack so we can allow the scasb
+; operator to use the rax register without fiddling.
+
+strlen_scasb:
+
+    enter 0,0		; Initialize the stack frame nicely for us
+
+    xor rax,rax		; Set the value that scasb will search for. In this case it is zero (the null terminator byte)
+    mov rcx,-1		; Store -1 in rcx so that scasb runs forever (or until it finds a null terminator). scasb DECREMENTS rcx each iteration
+    mov rdi,[rbp+16]	; Move the input string on the stack to rdi
+    cld			; Clear the direction flag so scasb iterates forward through the input string
+
+    repne scasb		; Execute the scasb instruction. This goes up to and includes the null terminator plus another decrement of rcx. The length is rcx-2.
+
+    not rcx		; Invert the value of rcx so that we get the two's complement value of the count. E.g, a count of -25 results in 24.
+    lea rax,[rcx-1]	; The above inversion includes the null terminator, so return the count-1 to strip it
 
     leave
     ret
