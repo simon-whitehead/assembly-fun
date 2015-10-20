6b73485d48ee9ac967b1d69035b3f0b36c2fb9cd
Simon Whitehead <chemnova@gmail.com>
2015-10-20 16:24:35 +1100

    Fixed up #2. Made the calling convention proper for 64 bit and passed in the length instead of it being magic

--- linux-x64/2.function-call/2.function-call.s
+++ linux-x64/2.function-call/2.function-call.s
@@ -10,7 +10,10 @@
 section .data
 
     msg1 db "Hello ", 0		; The first string to print,
+    msg1.len equ $-msg1		; Length of the first string
+
     msg2 db "world!", 10, 0	; The second string to print
+    msg2.len equ $-msg2		; Length of the second string
 
     sys_write equ 4		; long sys_write(unsigned int fd, const char __user *buf, size_t count);
     sys_exit equ 1
@@ -23,9 +26,11 @@ global _start
 
 _start:
 
-    mov rcx,msg1
+    mov rdi,msg1
+    mov rsi,msg1.len
     call .write_with_register
 
+    push msg2.len
     push msg2
     call .write_with_stack
 
@@ -34,29 +39,31 @@ _start:
 
     int 0x80
 
+; Args: (rdi: char*, rsi: int)
 .write_with_register:
 
     ; This is straight forward
 
     mov rax,sys_write
     mov rbx,stdout
-    mov rdx,6
+    mov rcx,rdi
+    mov rdx,rsi
 
     int 0x80
 
     ret
 
+; Args: (char*, int)
 .write_with_stack:
 
     ; Preserve the stack pointer .. just to be safe (not technically needed since we don't alter the stack in this routine)
     push rbp
     mov rbp,rsp
 
-    mov rcx,[rbp+16]		; First argument on the stack starts at +16 bytes (+8 for 32-bit - +0 = SP, +4 = return address, +8 = argument start)
-
     mov rax,sys_write
     mov rbx,stdout
-    mov rdx,7
+    mov rcx,[rbp+16]		; First argument on the stack starts at +16 bytes (+8 for 32-bit - +0 = SP, +4 = return address, +8 = argument start)
+    mov rdx,[rbp+24]		; Second argument is 8 bytes later
 
     pop rbp			; Restore the stack pointer
 
